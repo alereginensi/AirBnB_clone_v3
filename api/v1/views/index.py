8aa7e5476fa2dfe5595279ef966845abed955378
Agustin Labadie <labalabadie@gmail.com>
2022-09-10 22:42:20 +0000

    Trying task 5, different output because no entries

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,14 @@
 '''
 index
 '''
+from models.engine.file_storage import classes
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
@@ -11,5 +19,10 @@ def index():
     """returns status"""
     return jsonify({"status": "OK"})
 
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """Calls count method"""
+    return jsonify(storage.count(classes['Amenity']))
 # if __name__ == '__main__':
 #    app.run(host='0.0.0.0', port='5000')
