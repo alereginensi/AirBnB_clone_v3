20a89cdc47edca69c69e4c2438673418567053d0
alereginensi <reginensia@gmail.com>
2022-09-11 16:28:34 -0700

    fixed ports

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ objects that handles all default RestFul API actions for cities """
-from models.city import city
+from models.city import City
 from models.state import State
 from models import storage
 from api.v1.views import app_views
